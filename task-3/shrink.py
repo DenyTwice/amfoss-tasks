001e6d79e3c6ce8bf49b6cdbe0496167f94fde11
LiquidFlare <ivinjoelabraham@gmail.com>
2022-11-06 20:36:40 +0530

    added shrink.py

---
+++ task-3/shrink.py
@@ -0,0 +1,13 @@
+def findSum(n):
+    lstN = list(map(int, str(n)))
+    sum = 0
+    for i in range(len(lstN)):
+        sum += lstN[i]
+    return sum
+
+num = input()
+rounds = 0
+while int(num) != 1:
+    num = findSum(num)
+    rounds += 1
+print(rounds)
