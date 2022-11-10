ee47405c724a94276bb64a57f1e84c1916d0c6c2
LiquidFlare <ivinjoelabraham@gmail.com>
2022-11-10 21:43:38 +0530

    Task 8 done

---
+++ task-8/index.js
@@ -0,0 +1,7 @@
+let playW = () => new Audio("sounds/crash.mp3").play()
+let playA = () => new Audio("sounds/kick-bass.mp3").play()
+let playS = () => new Audio("sounds/snare.mp3").play()
+let playD = () => new Audio("sounds/tom-1.mp3").play()
+let playJ = () => new Audio("sounds/tom-2.mp3").play()
+let playK = () => new Audio("sounds/tom-3.mp3").play()
+let playL = () => new Audio("sounds/tom-4.mp3").play()
