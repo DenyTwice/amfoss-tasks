5fda35e19591957afbe3896a22944ac45bede248
LiquidFlare <ivinjoelabraham@gmail.com>
2022-12-07 04:11:03 +0530

    Did Monster Slayer in T3

---
+++ task-03/monster.py
@@ -0,0 +1,17 @@
+t = int(input())
+for i in range(t):
+    num = int(input())
+    list1 = list(map(int, input().split()))
+    for i in range(num):
+        try:
+            if list1[i+1] % list1[i] == 0:
+                list1[i+1] = list1[i]
+            else:
+                print("NO")
+                break
+        except IndexError:
+            if set(list1) == {list1[0]}:
+                print("YES")
+            else:
+                print("NO")
+    
